6738985a8eee693c379c5b9c98052ec9f13b1bb5
randoguyname <43623624+randoguyname@users.noreply.github.com>
2021-08-16 22:08:23 +1000

    used safe object references as opposed to a fully featured default object as state in newsitemscreen

--- app/screens/NewsItemScreen.js
+++ app/screens/NewsItemScreen.js
@@ -13,7 +13,7 @@ import ContentScreenTemplate, { HorizontalRule, HTMLTextView } from "./ContentSc
 class NewsItemScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = { bodyDone: false, body: { article: { author: { fullName: "", _links: { profile: { href: "" } } }, title: "", featureImage: { _links: { image: { href: "" } } } } } };
+        this.state = { bodyDone: false, body: {} };
     }
     componentDidMount = () => {
         if (this.props.route.params.id) {
@@ -33,21 +33,37 @@ class NewsItemScreen extends Component {
             >
                 <View style={{ backgroundColor: customColours.contentBackground, marginBottom: 70 }}>
                     <LoaderComponent style={{ padding: 0 }} state={this.state.bodyDone ? "loaded" : "loading"}>
-                        {this.state.body.article.featureImage ? <Image
-                            source={{ uri: (this.state.body.article.featureImage._links.image.href.startsWith("https") ? "" : serviceURL) + this.state.body.article.featureImage._links.image.href }}
+                        {this.state.body?.article?.featureImage ? <Image
+                            source={{
+                                uri: (
+                                    this.state.body?.article?.featureImage?._links?.image?.href?.startsWith?.("https")
+                                        ? ""
+                                        : serviceURL
+                                ) + this?.state?.body?.article?.featureImage?._links?.image?.href
+                            }}
                             style={{ width: "100%", height: 250 }}
                             resizeMode='cover'
                         /> : null}
                         <View style={{ paddingHorizontal: 15, paddingTop: 15, paddingBottom: 10 }}>
-                            <ContentText style={styles.contentHeading}>{this.state.body.article.title}</ContentText>
+                            <ContentText style={styles.contentHeading}>
+                                {this.state.body?.article?.title}
+                            </ContentText>
                             <UserLinkComponent
                                 textBefore={newsItemAuthorLabel}
                                 isMeta={true}
-                                userName={this.state.body.article.author.fullName}
-                                id={this.state.body.article.author._links.profile.href.match(/\d+/g)}
+                                userName={this.state.body?.article?.author?.fullName}
+                                id={this.state.body?.article?.author?._links?.profile?.href?.match?.(/\d+/g)}
                             />
-                            <HorizontalRule></HorizontalRule>
-                            <HTMLTextView style={{ paddingHorizontal: 5, paddingTop: 5, marginBottom: 0 }}>{this.state.body.article.body}</HTMLTextView>
+                            <HorizontalRule />
+                            <HTMLTextView
+                                style={{
+                                    paddingHorizontal: 5,
+                                    paddingTop: 5,
+                                    marginBottom: 0
+                                }}
+                            >
+                                {this.state.body?.article?.body}
+                            </HTMLTextView>
                         </View>
                     </LoaderComponent>
                 </View>
